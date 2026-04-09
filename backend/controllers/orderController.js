13ca32ed228fba8c11e27ad806a4ef309bbca1df
Sinduja R <166295827+MyAppCod@users.noreply.github.com>
2026-04-09 15:45:27 +0530

    Add files via upload

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -29,10 +29,9 @@ exports.placeOrder = async (req, res) => {
     const customOrderId = `ORD-${Date.now()}`;
 
     // ✅ Generate QR Code (UPI payment link)
-    const upiLink = `upi://pay?pa=${process.env.UPI_ID}&pn=Store&am=${total}&cu=INR`;
+    const upiLink = `upi://pay?pa=${process.env.UPI_ID}&pn=Vignesh&mc=${total}mode=02&purpose=00`;
 
-   const qrImage = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(upiLink)}`;
-
+const qrImage = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${encodeURIComponent(upiLink)}`;
     const order = new Order({
        user: req.user.id,
       orderId: customOrderId,
