566af44ea5963ec366c8b95dffb2a08a93cafe91
Sinduja R <166295827+MyAppCod@users.noreply.github.com>
2026-04-13 23:02:32 +0530

    Add files via upload

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,4 +13,20 @@ router.get('/', auth, role('admin'), getAllUsers);
 router.put('/:id/role', auth, role('admin'), updateRole);
 router.delete('/:id', auth, role('admin'), deleteUser);
 
+
+// ✅ GET USER PROFILE
+router.get('/profile', authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    if (!user) {
+      return res.status(404).json({ msg: 'User not found' });
+    }
+
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
